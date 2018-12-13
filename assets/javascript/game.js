7042b735bcc2ba018324cffa9131485e75abb465
shinyscyther <mesibovj@gmail.com>
2018-12-13 14:53:53 -0500

    created the alphabet array, established win, loss, and guesses variables, and pseudocoded the js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// declare the variable "alphabet" as an array of the letters of the alphabet
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// declare the variables "wins", "losses", and "guesses"
+var wins = 0;
+var losses = 0;
+var guesses = 0;
+
+// set up the computer choice guess
+
+
+// set up the function that checks what happens if the user's guess matches the computer's guess (the win condition)
+
+
+//     as part of that same function, make the else statement do the following: Reduce the guesses left by one (starting from 7)
+
+
+// set up the function that checks what happens if the user's guesses reach 0 (the loss condition)
